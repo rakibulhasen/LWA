eb14e2274ef6ebd6190debc7dc3f222cb8c0b73f
Rakibul Hasan <rakibul.4658@gmail.com>
2025-06-30 07:05:07 +0600

    first commit

--- old_class_10/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'header.php';
-
-if(isset($_SESSION['admin'])) {
-    unset($_SESSION['admin']);
-    $_SESSION['success_message'] = "Logout Successfull";
-    header('location: login.php');
-} 
